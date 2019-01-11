216ab9eb1a71ee621e9577d03000e7010ef53040
MohammedHajaj <45947554+MohammedHajaj@users.noreply.github.com>
2019-01-11 11:45:26 +0000

    Add files via upload

---
+++ CNN_Variable_Input_Size/Header.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<iostream>
+#include "cuda_runtime.h"
+#include <cublas_v2.h>
+#include <stdio.h>
+#include<random>
+#include "curand.h"
+#include"cudnn.h"
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+
+#include"ConstantSettings.h"
+#include"Functions.h"
+
+using namespace std;
+
