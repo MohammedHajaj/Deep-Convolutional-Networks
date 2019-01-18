56c448d97a305ca054a667be78a0ffde08dfa6fc
MohammedHajaj <45947554+MohammedHajaj@users.noreply.github.com>
2019-01-18 11:28:04 +0000

    Add files via upload

---
+++ CNN_MultiTask/Functions.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+//==============================================================================================================================================================================================================
+// Function declarations for Functions.cpp
+//==============================================================================================================================================================================================================
+
+void DividDatasetIntoMultipleTasks(int **, int **, int ****, int ***, int *, int *, int *, int *, int, int);
+
+void DividTestsetIntoMultipleTasks(unsigned int **, int **, int ***, int **, int *, int *, int, curandGenerator_t);
+
+void ReshuffleImagesMT(int *, int *, int ***, int **, int *, int);
+
+void SaveParameters1(FILE *, float *, float **, float **, float *, float **, int *, int *, int);
+
+void ReloadParameters1(FILE *, float *, float **, float **, float *, float **, int *, int *, int);
+
+void SaveParameters2(FILE *, float *, float **, float **, float **, float **, float *, float *, float **, float **, int *, int *, int, int, int, int, float);
+
+void ReloadParameters2(FILE *, float *, float **, float **, float **, float **, float *, float *, float **, float **, int *, int *, int, int *, int *, int *, float *);
+
+void InitializeTrainingData(unsigned int **, unsigned int **, size_t **, int **, int **, size_t **, size_t **, int *, int *, int);
+
+void InitialzeCuDNN(cudnnTensorDescriptor_t *, cudnnTensorDescriptor_t *, cudnnFilterDescriptor_t *, cudnnConvolutionDescriptor_t *, cudnnTensorDescriptor_t *, cudnnTensorDescriptor_t *, cudnnFilterDescriptor_t *, cudnnConvolutionDescriptor_t *, cudnnConvolutionFwdAlgo_t *, cudnnConvolutionBwdDataAlgo_t *, cudnnConvolutionBwdFilterAlgo_t *, int *, int *, int *);
+
+void AllocateParamGPUMemory(float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float ***, float **, float **, float **, float **, float **, int ***, bool ***, int *, int *, int *, int *, int *, int *, int *, int);
+
+void AllocateAuxiliaryGPUMemory(float ***, float **, float **, float **, float **, float **, float **, unsigned int **, float **, float **, int *, int *, int *, int, int);
+
+void InitializeConvLayerParam(int *, int *, int *, int *, int *, int *, int *, int *, int *, float **);
+
+void ParameterInitialization(float **, float **, float *, float **, float *, int *, int *, int);
+
+void InitializeCudaKernels(dim3 *, dim3 *, dim3 *, dim3 *, dim3 *, dim3 *, dim3 *, dim3 *, int *, int *, int *, int *, int *, int *, int *, int *, int *, int);
+
+void PrintIterResults(FILE *, float *, float *, int *, int, int, int, int, int);
+
+void InitializeMultiCropInference(int **);
+
+void PrintFinalResults(FILE *, float *, float *, int *);
+
+void FreeTrainSpecificData(float **, float **, float **, float **, float **, float **, float **, float *, float *, float *, float *, float *, float *, int *, int ***, int *, unsigned int *, int);
+
+void FreeRemainingMem(unsigned char *, unsigned char *, unsigned char *, float **, float **, float **, float **, float **, float **, float **, int **, bool **, float *, float *, float *, float *, float *, float *, float *, unsigned int *, unsigned int *, size_t *, int *, int *, size_t *, size_t *, float *);
+
+static int GridSizeAC(const int, const int);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
